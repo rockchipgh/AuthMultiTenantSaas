f232000657378d042985ddc4c61df6a4a2c9ed4a
Rohit P S <rohithp61@gmail.com>
2024-09-06 17:05:16 +0530

    Added yaml for config storage

--- pgSchemaCreate.py
+++ pgSchemaCreate.py
@@ -1,19 +1,33 @@
+import yaml
 import psycopg2
 
+with open("config.yaml") as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
 conn = psycopg2.connect(database='postgres',
-                        user='postgres',
-                        host='localhost',
-                        password='root',
-                        port=5432)
+                        user=config['postgres']['user'],
+                        host=config['postgres']['host'],
+                        password=config['postgres']['password'],
+                        port=config['postgres']['port'])
 conn.autocommit = True
 cur = conn.cursor()
-sql = 'CREATE DATABASE UAT;'
+sql = f"CREATE DATABASE {config['postgres']['database']};"
 try:
     cur.execute(sql)
 except psycopg2.errors.DuplicateDatabase:
     print("Database already exists")
 except:
     print("Unknown Error while creating database")
-
-
 conn.close()
+
+conn = psycopg2.connect(database='postgres',
+                        user=config['postgres']['user'],
+                        host=config['postgres']['host'],
+                        password=config['postgres']['password'],
+                        port=config['postgres']['port'])
+conn.autocommit = True
+cur = conn.cursor()
+
